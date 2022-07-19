b883ea6acb178be04181c5552de8bedc9eba1a7b
Teslim Akinsade <>
2022-07-19 00:55:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+# This python file will create band name Generator
+print("Welcome to band name generator")
+band_name=input("what city did you grow up?\n")
+pet_name=input("what is the name of your pet?\n")
+print("Your band name could be " + band_name + " " + pet_name)
